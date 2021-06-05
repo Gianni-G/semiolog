acb599904e6d94e3c681c3e4616f1b16314c8907
Gianni <juan.luis.gastaldi@gess.ethz.ch>
2021-06-05 19:20:37 +0200

    more paradigm

--- semiolog/paradigm.py
+++ semiolog/paradigm.py
@@ -1,12 +1,17 @@
-# from transformers import pipeline
-# from tqdm.notebook import tqdm, trange
 from thinc.api import Config
+from scipy.stats import entropy
+import numpy as np
 
-# Loading the semiotic configs here is a roundabout not to load the unmasker at each execution of the chain_paradigm. This should be solved otherwise
-# semiotic = Config().from_disk(paths.corpora / name / "config.cfg")
-
-
-# unmasker = pipeline('fill-mask', model='distilbert-base-uncased',top_k=10)
+class Parad:
+    def __init__(self,parad:dict) -> None:
+        self.len = len(parad)
+        self.keys = tuple(parad.keys())
+        self.values = np.array(list(parad.values()))
+        self.mass = np.sum(self.values)
+        self.entropy = entropy(self.values)
+    
+    def __repr__(self) -> str:
+        return str(self.keys)
 
 def chain_paradigm(chain,semiotic, unmasker, thres=0):
 
@@ -14,8 +19,15 @@ def chain_paradigm(chain,semiotic, unmasker, thres=0):
 
     sent_list = chain.split
     sent_mask = [" ".join([token if n!=i else "[MASK]" for n,token in enumerate(sent_list)]) for i in range(len(sent_list))]
-    parads = [{i['token_str']:i['score'] for i in unmasker(sent) if i['score']>thres and i['token_str'] not in {
+
+    punctuation = {
         ".",":",",","…","'","’","′",'"',"•",";","`","-","“","...","?","!","/","&","–"
-        } } for sent in sent_mask]
+        }
+
+    parads = []
+    for sent in sent_mask:
+        parad = {i['token_str']:i['score'] for i in unmasker(sent) if i['score']>thres and i['token_str'] not in punctuation }
+
+        parads.append(Parad(parad))
 
     return parads
