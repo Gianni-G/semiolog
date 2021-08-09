e10e286367024551ab877adb2b8318dbf2dd1bc5
Gianni <juan.luis.gastaldi@gess.ethz.ch>
2021-08-09 18:59:15 +0200

    new build with delta pair_len

--- semiolog/vocabulary.py
+++ semiolog/vocabulary.py
@@ -237,8 +237,9 @@ class Vocabulary:
                 jobs_data = parallel_pool(delayed(pre_process)(chunk,self.normalizer.normalize) for chunk in corpus_chunks)
 
                 pair_len_global = reduce(operator.add,[i[-1] for i in jobs_data])
-                best_pair, best_pair_len = pair_len_global.most_common(1)[0]
-                
+
+                best_pair, best_pair_len = max(pair_len_global.items(), key=operator.itemgetter(1))
+
                 merges = [" ".join(best_pair)]
                 print(f"... computed in {time.time()-start} secs.\n")
 
@@ -287,8 +288,8 @@ class Vocabulary:
                         pair_len_global = reduce(operator.add,[i[-1] for i in jobs_data])
                     else:
                         pair_len_global = reduce(operator.add,[Counter(dict(i[-1].most_common(truncate_best_size))) for i in jobs_data])
-                    best_pair, best_pair_len = pair_len_global.most_common(1)[0]
-
+                    
+                    best_pair, best_pair_len = max(pair_len_global.items(), key=operator.itemgetter(1))
 
                     merges.append(" ".join(best_pair))
                     # print(f"... computed in {time.time()-start} secs.\n")
@@ -330,8 +331,8 @@ class Vocabulary:
             job_data = pre_process(corpus_chain,self.normalizer.normalize)
 
             pair_len_global = job_data[-1]
-            best_pair, best_pair_len = pair_len_global.most_common(1)[0]
-            
+            best_pair, best_pair_len = max(pair_len_global.items(), key=operator.itemgetter(1))
+
             merges = [" ".join(best_pair)]
             print(f"... computed in {time.time()-start} secs.\n")
 
@@ -376,7 +377,7 @@ class Vocabulary:
                 job_data = process_best_pair(job_data, best_pair)
 
                 pair_len_global = job_data[-1]
-                best_pair, best_pair_len = pair_len_global.most_common(1)[0]
+                best_pair, best_pair_len = max(pair_len_global.items(), key=operator.itemgetter(1))
 
                 merges.append(" ".join(best_pair))
                 # print(f"... computed in {time.time()-start} secs.\n")
@@ -430,6 +431,300 @@ class Vocabulary:
             self.save()
             print(f"Vocabulary saved to {self.path}")
 
+    def build_new(
+        self,
+        corpus = None,
+        vocab_size = None,
+        special_tokens = None,
+        save = False,
+        save_step = None,
+        progress_bar = True,
+        resume_merges = False,
+        parallel = False,
+        corpus_length = None
+        ):
+
+        if corpus == None:
+            corpus = self.name
+        
+        if vocab_size == None:
+            vocab_size = self.config.size
+
+        if special_tokens == None:
+            special_tokens = self.config.special_tokens
+        
+        if corpus_length == None:
+            corpus_length = self.corpus.train_len
+        
+        if save == True and save_step != None:
+            saveQ = True
+            
+            if not isdir(self.path):
+                makedirs(self.path)
+                
+        else:
+            saveQ = False
+
+        def pre_process(corpus_chunk, normalizer):
+            # Normalize
+            chain_zip = normalizer(corpus_chunk)
+            # Build list of pairs
+            chain_zip = list(zip(chain_zip,chain_zip[1:]))
+            # Create a lookup table of all the positions where a pair appears in a corpus
+            pair_pos = defaultdict(set)
+            for i,k in list(enumerate(chain_zip)):
+                pair_pos[k].add(i)
+            # From the previous lookup table, create another lookup table of the frequency of each pair (given by the size of the set of its positions)
+            pair_len = Counter()
+            for k,pos in pair_pos.items():
+                pair_len[k] = len(pos)
+            
+            return (chain_zip, pair_pos, pair_len)
+
+        def process_best_pair(chain_zip, pair_pos, best_pair):
+            chain_zip_len = len(chain_zip)
+            pair_len_delta = Counter()
+
+            for i in pair_pos[best_pair]:
+                # Skip iteration if position corresponds to a modified set of positions during the iteration. This can happen if there is overlap of pairs, such as "000", where ("0","0") has itself as right pair. Note that, due to unordered implementation of sets, this entails a lack of systematicity in overlapping cases: "000" can be counted randomly as ("00","0") or ("0","00").
+                # TODO: Investigate the cost of ordering sets. In which case, the following "if" condition might only be needed for right pairs.
+                if chain_zip[i]!=best_pair:
+                    continue
+                ## merge best pair with left unit
+                left_pair_i = i-1
+                while left_pair_i>=0 and chain_zip[left_pair_i] == None: # if left pair is within chain limits but empty (= None) because already merged previously, shift to the left
+                    left_pair_i -= 1
+                if left_pair_i>-1: # proceed only if a left pair was found on the left
+                    # Remove from left pair positions, the current position (of the pair to be merged)
+                    left_pair = chain_zip[left_pair_i]
+                    # Skip update of left_pair position set if left_pair = best_pair, to avoid modification of iterating set. This can happen if there is overlap of pairs. No consequences on final result (right?) since right after the loop, the key corresponding to the best pair is deleted, and chain_zip is indeed updated so the problematic cases can be captured at the beginning of the loop.
+                    if left_pair != best_pair:
+                        left_pair_pos = pair_pos[left_pair]
+                        left_pair_pos.discard(left_pair_i)
+                    new_pair = (left_pair[0],"".join(best_pair)) # construct new left pair
+                    pair_pos[new_pair].add(left_pair_i) # add new pair (if non existing) and its position to the pair_pos lookup table
+                    # update the counts in the pair_len lookuptable
+                    pair_len_delta[left_pair] -= 1
+                    pair_len_delta[new_pair] += 1
+
+                    # update the list of pairs
+                    chain_zip[left_pair_i] = new_pair
+
+                ## merge best pair with right unit.
+                # Code is symmetric to left_pair but on the right. Comments are omitted
+                right_pair_i = i+1
+                while right_pair_i<chain_zip_len and chain_zip[right_pair_i] == None:
+                    right_pair_i += 1
+                if right_pair_i<chain_zip_len:
+                    right_pair = chain_zip[right_pair_i]
+                    if right_pair != best_pair:
+                        right_pair_pos = pair_pos[right_pair]
+                        right_pair_pos.discard(right_pair_i)
+                    new_pair = ("".join(best_pair), right_pair[1])
+                    pair_pos[new_pair].add(right_pair_i)
+                    pair_len_delta[right_pair] -= 1
+                    pair_len_delta[new_pair] += 1
+
+                    chain_zip[right_pair_i] = new_pair
+
+                # Empty best pair position in list of pairs
+                chain_zip[i] = None
+
+            # Remove best pair from lookuptables
+            del pair_pos[best_pair]
+
+            return (chain_zip, pair_pos, pair_len_delta)
+
+        def compute_freq(chain_zip):
+            # TODO: add the last unit to the decoupling
+            freq = [pair[0] for pair in chain_zip if pair != None]
+            if chain_zip[-1]!=None: 
+                freq.append(chain_zip[-1][-1])
+            freq = Counter(freq)
+            return freq
+        
+
+        if parallel:
+            chunksize = int(corpus_length/self.cpu_count)
+
+            corpus_chunks = ["".join(self.corpus.train[i*chunksize:i*chunksize+chunksize]) for i in range(0,self.cpu_count)]
+
+            with Parallel(n_jobs=self.cpu_count, require='sharedmem') as parallel_pool:
+                print("Computing in parallel")
+                print("Normalize and jobs data...")
+                start = time.time()
+                jobs_data = parallel_pool(delayed(pre_process)(chunk,self.normalizer.normalize) for chunk in corpus_chunks)
+
+                pair_len_global = reduce(operator.add,[i[-1] for i in jobs_data])
+
+                best_pair, best_pair_len = max(pair_len_global.items(), key=operator.itemgetter(1))
+                
+                merges = [" ".join(best_pair)]
+                print(f"... computed in {time.time()-start} secs.\n")
+
+                print("Build alphabet...")
+                start = time.time()
+                alphabet = Counter()
+                for (l,r),v in pair_len_global.items():
+                    alphabet[l] += v
+                # In extreme cases, right characters of pairs might not be left characters. If there are such chars, they're added with freq 1
+                left_out_chars = {r for l,r in pair_len_global.keys()}-alphabet.keys()
+                if len(left_out_chars)>0:
+                    print(f"Adding characters: {left_out_chars}")
+                    for char in left_out_chars:
+                        alphabet[char] += 1
+                print(f"... computed in {time.time()-start} secs.\n")
+
+                alpha_len = len(alphabet)
+                special_tokens_len = 0 if special_tokens == None else len(special_tokens)
+                
+                print(f"Alphabet Size: {alpha_len}")
+                print(f"Special Tokens Size: {special_tokens_len}")
+
+                
+                if vocab_size<0:
+                    voc_final_length = alpha_len + abs(vocab_size) + special_tokens_len
+                else:
+                    voc_final_length = vocab_size
+
+                delta_voc = voc_final_length - alpha_len - special_tokens_len
+
+                print(f"Terms to compute: {delta_voc}\n")
+
+                print("Enter loop")
+
+                t = trange(delta_voc, disable = not progress_bar)
+                for _ in t:
+                    t.set_description(f"Pair: {best_pair}, {best_pair_len}")
+                    t.refresh()
+
+                    jobs_data = parallel_pool(delayed(process_best_pair)(chain_zip, pair_pos, best_pair) for chain_zip, pair_pos, pair_len_delta in jobs_data)
+
+                    for chain_zip, pair_pos, pair_len_delta in jobs_data:
+                        pair_len_global.update(pair_len_delta)
+
+                    # Remove best_pair from pair_len
+                    del pair_len_global[best_pair]
+
+                    best_pair, best_pair_len = max(pair_len_global.items(), key=operator.itemgetter(1))
+
+                    merges.append(" ".join(best_pair))
+                    # print(f"... computed in {time.time()-start} secs.\n")
+
+                print("Compute freq...")
+                start = time.time()
+                freqs = parallel_pool(delayed(compute_freq)(job_data[0]) for job_data in jobs_data)
+                freq = reduce(operator.add, freqs)
+                print(f"... computed in {time.time()-start} secs.\n")
+        
+        else:
+            #TODO: Sequential computing not tested
+            print("Computing sequentially")
+            print("Normalize and jobs data...")
+            start = time.time()
+            corpus_chain = "".join(self.corpus.train[:corpus_length])
+            chain_zip, pair_pos, pair_len_global = pre_process(corpus_chain,self.normalizer.normalize)
+
+            best_pair, best_pair_len = max(pair_len_global.items(), key=operator.itemgetter(1))
+            
+            merges = [" ".join(best_pair)]
+            print(f"... computed in {time.time()-start} secs.\n")
+
+            print("Build alphabet...")
+            start = time.time()
+            alphabet = Counter()
+            for (l,r),v in pair_len_global.items():
+                alphabet[l] =+ v
+            # In extreme cases, right characters of pairs might not be left characters. If there are such chars, they're added with freq 1
+            left_out_chars = {r for l,r in pair_len_global.keys()}-alphabet.keys()
+            if len(left_out_chars)>0:
+                print(f"Adding characters: {left_out_chars}")
+                for char in left_out_chars:
+                    alphabet[char] =+ 1
+            print(f"... computed in {time.time()-start} secs.\n")
+
+            alpha_len = len(alphabet)
+            special_tokens_len = 0 if special_tokens == None else len(special_tokens)
+            
+            print(f"Alphabet Size: {alpha_len}")
+            print(f"Special Tokens Size: {special_tokens_len}")
+            
+            if vocab_size<0:
+                voc_final_length = alpha_len + abs(vocab_size) + special_tokens_len
+            else:
+                voc_final_length = vocab_size
+
+            delta_voc = voc_final_length - alpha_len - special_tokens_len
+            
+            print(f"Terms to compute: {delta_voc}\n")
+
+            print("Enter loop")
+
+            # for _ in trange(delta_voc):
+            t = trange(delta_voc, disable = not progress_bar)
+            for _ in t:
+                t.set_description(f"Pair: {best_pair}, {best_pair_len}")
+                t.refresh()
+
+                chain_zip, pair_pos, pair_len_delta = process_best_pair(chain_zip, pair_pos, best_pair)
+
+                # Remove best_pair from pair_len
+                pair_len_global.update(pair_len_delta)
+
+                del pair_len_global[best_pair]
+                best_pair, best_pair_len = max(pair_len_global.items(), key=operator.itemgetter(1))
+
+                merges.append(" ".join(best_pair))
+                # print(f"... computed in {time.time()-start} secs.\n")
+
+                if saveQ == True:
+                    voc_partial_len = alpha_len + special_tokens_len + _ + 1
+                    if voc_partial_len % save_step == 0 and voc_partial_len != voc_final_length:
+
+                        print("Saving intermediate results...")
+                        start = time.time()
+                        freq = compute_freq(job_data[0])
+
+                        vocabulary = freq.most_common()
+                        
+                        if special_tokens != None:
+                            vocabulary = vocabulary + [(token,0) for token in special_tokens]
+                        
+                        self.merges = merges
+                        self.encode = {k:i for i,(k,v) in enumerate(vocabulary)}
+                        self.freq = dict(vocabulary)
+                        self.alpha = dict(alphabet.most_common())
+                        step_path = self.path / str(voc_partial_len)
+                        self.save(step_path)
+                        print(f"... computed in {time.time()-start} secs.")
+                        print(f"Intermediate vocabulary saved to {step_path}\n")
+            
+            print("Compute freq...")
+            start = time.time()
+            freq = compute_freq(chain_zip)
+            print(f"... computed in {time.time()-start} secs.\n")
+
+        vocabulary = freq.most_common()
+        
+        if special_tokens != None:
+            vocabulary = vocabulary + [(token,0) for token in special_tokens]
+        
+        self.merges = merges
+        self.encode = {k:i for i,(k,v) in enumerate(vocabulary)}
+        self.freq = dict(vocabulary)
+        self.alpha = dict(alphabet.most_common())
+
+        self.decode = {i:k for k,i in self.encode.items()}
+        
+        self.len = len(vocabulary)     
+        self.freq_mass = sum(self.freq.values())
+        self.prob = {k:v/self.freq_mass for k,v in self.freq.items()}
+
+        print("Vocabulary built")
+        
+        if save == True:
+            self.save()
+            print(f"Vocabulary saved to {self.path}")
 
     def save(self, path = None):
         
