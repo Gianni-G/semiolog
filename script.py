a0e45bf4ea4def4abec251aabb84df6f9d835e5d
Gianni-MB <juan.luis.gastaldi@gess.ethz.ch>
2022-03-23 00:23:21 +0100

    script and HF Offline

--- script.py
+++ script.py
@@ -5,4 +5,4 @@ semiotic = slg.Cenematic("en_bnc",requested_cpu = 32)
 print(f"Numbers of cores: {semiotic.config.system.cpu_count}")
 
 print("\nSequential")
-semiotic.vocab.build_new(save = True, parallel = True, save_step=1000)
+semiotic.vocab.build(save = True, parallel = True, save_step=1000)
