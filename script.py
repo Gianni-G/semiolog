04c3058c1ad7c34be7c1ba900106faf7d58feab4
Gianni-MB <juan.luis.gastaldi@gess.ethz.ch>
2022-03-23 09:48:30 +0100

    script

--- script.py
+++ script.py
@@ -4,4 +4,6 @@ semiotic = slg.Cenematic("en_bnc_test",requested_cpu = 4)
 
 print(f"Numbers of cores: {semiotic.config.system.cpu_count}")
 
+semiotic.corpus.build(save = True)
+
 semiotic.vocab.build(save = True, parallel = True, save_step=20)
