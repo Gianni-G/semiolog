e0d40079321c1c7b7a12144274078a847508ba3f
Gianni iMac ETH <juan.luis.gastaldi@gess.ethz.ch>
2022-03-25 16:30:51 +0100

    script

--- script.py
+++ script.py
@@ -4,6 +4,6 @@ semiotic = slg.Cenematic("en_bnc_test",requested_cpu = 32)
 
 print(f"Numbers of cores: {semiotic.config.system.cpu_count}")
 
-semiotic.corpus.build(save = True)
+# semiotic.corpus.build(save = True)
 
 semiotic.vocab.build(save = True, parallel = True, save_step=200)
