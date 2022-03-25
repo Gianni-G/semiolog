7fbb46064be6857d1f047abdd4b7f9a32e2590d7
Gianni iMac ETH <juan.luis.gastaldi@gess.ethz.ch>
2022-03-25 16:15:49 +0100

    script

--- script.py
+++ script.py
@@ -1,9 +1,9 @@
 import semiolog as slg
 
-semiotic = slg.Cenematic("en_bnc_test",requested_cpu = 4)
+semiotic = slg.Cenematic("en_bnc_test",requested_cpu = 32)
 
 print(f"Numbers of cores: {semiotic.config.system.cpu_count}")
 
 semiotic.corpus.build(save = True)
 
-semiotic.vocab.build(save = True, parallel = True, save_step=20)
+semiotic.vocab.build(save = True, parallel = True, save_step=200)
