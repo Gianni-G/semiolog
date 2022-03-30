8538f6b6389fb0ea44dd469464111c6029afabf2
Gianni-MB <juan.luis.gastaldi@gess.ethz.ch>
2022-03-30 16:40:16 +0200

    debug

--- script.py
+++ script.py
@@ -4,4 +4,12 @@ semiotic = slg.Cenematic("en_bnc")
 
 print(f"Numbers of cores: {semiotic.config.system.cpu_count}")
 
-semiotic.paradigmatic.build()
+# semiotic.paradigmatic.build()
+
+from datasets import DatasetDict, Dataset
+semiotic.paradigmatic.build(dataset=DatasetDict({
+    "train": Dataset.from_dict(semiotic.corpus.dataset["train"][:1000]),
+    "dev": Dataset.from_dict(semiotic.corpus.dataset["dev"][:100]),
+    "test": Dataset.from_dict(semiotic.corpus.dataset["test"][:100])
+})
+)
