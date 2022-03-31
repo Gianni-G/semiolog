6947fa239b968315d784c31ca17a70f39c073285
Gianni-MB <juan.luis.gastaldi@gess.ethz.ch>
2022-03-31 09:43:40 +0200

    environ TF_GPU_ALLOCATOR

--- semiolog/__init__.py
+++ semiolog/__init__.py
@@ -3,6 +3,7 @@ from os import environ
 # TODO: provide access to modification of these parameters
 environ['TRANSFORMERS_OFFLINE'] = "1"
 environ['HF_DATASETS_OFFLINE'] = "1"
+environ['TF_GPU_ALLOCATOR'] = "cuda_malloc_async"
 
 from typing import Union, Iterable, Dict, Any
 from pathlib import Path
