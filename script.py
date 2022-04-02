1ec95a92bdb94d531da6f4166c3bc486d36ae891
Gianni-MB <juan.luis.gastaldi@gess.ethz.ch>
2022-04-02 16:03:43 +0200

    script

--- script.py
+++ script.py
@@ -7,8 +7,8 @@ print(f"Numbers of cores: {semiotic.config.system.cpu_count}")
 import datasets
 tokenized_datasets = datasets.load_from_disk(semiotic.paths.paradigms / "tokenized")
 tokenized_datasets = datasets.DatasetDict({
-    "train":tokenized_datasets["train"].select(range(100000)),
-    "dev": tokenized_datasets["dev"].select(range(5000))
+    "train":tokenized_datasets["train"].select(range(10000)),
+    "dev": tokenized_datasets["dev"].select(range(500))
 })
 
 semiotic.paradigmatic.build(dataset=tokenized_datasets, load_tokenized=True, save_tokenized=True)
