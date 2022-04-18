4393dd7eb33b3cb4fd2071fcc03184f49d3fca9e
Gianni-EULER <juan.luis.gastaldi@gess.ethz.ch>
2022-04-18 14:39:41 +0200

    script old seg test

---
+++ script_olds_test.py
@@ -0,0 +1,24 @@
+import semiolog as slg
+
+semiotic = slg.Cenematic("en_bnc_old_seg_test")
+
+# print(f"Numbers of cores: {semiotic.config.system.cpu_count}")
+
+# semiotic.corpus.build(
+#     save = True,
+#     )
+
+# semiotic.vocab.build(
+#     save = True,
+#     parallel = True,
+#     save_step = 1000,
+#     )
+
+# semiotic.syntagmatic.build()
+
+semiotic.paradigmatic.build(
+    n_sents=100000,
+    load_tokenized = False,
+    checkpoints=True,
+    min_token_length = 3,
+    )
