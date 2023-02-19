879f5412578517c76ce9e8087b1f8ac6591f0d53
Gianni-MB <juan.luis.gastaldi@gess.ethz.ch>
2023-02-18 23:39:22 -0500

    furo theme for docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'semiolog'
 copyright = '2023, Gianni Gastaldi'
 author = 'Gianni Gastaldi'
-release = '0.1'
+release = '0.0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -24,5 +24,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'furo'
 html_static_path = ['_static']
