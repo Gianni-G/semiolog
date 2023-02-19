3032e51678a82a8369b07970bf49cde6d7cdf510
Gianni-MB <juan.luis.gastaldi@gess.ethz.ch>
2023-02-19 01:35:15 -0500

    pyproject & setup

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='semiolog',
-      version='0.1',
+      version='0.0.0',
       description='Semiological analysis',
       url='https://gitlab.ethz.ch/semiomaths/semiolog',
-      author='Juan Luis Gastaldi',
-      author_email='juan.luis.gastaldi@gess.ethz.ch',
+      author='Gianni Gastaldi',
+      author_email='giannigastaldi@gmail.com',
       license='CC BY-NC 4.0',
       packages=['semiolog'],
       zip_safe=False)
