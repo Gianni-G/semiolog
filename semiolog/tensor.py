0765181cd54275fcdb1f37f80d1f0978db011796
Gianni-MB <juan.luis.gastaldi@gess.ethz.ch>
2023-03-08 01:56:31 +0100

    bye quimb

--- semiolog/tensor.py
+++ semiolog/tensor.py
@@ -1,6 +1,6 @@
 
 import numpy as np
-import quimb.tensor as qtn
+import jax.numpy as jnp
 from .util import plot_hm
 
 class Tensor():
@@ -28,28 +28,45 @@ class Tensor():
         M_norm = M_raw/M_raw.sum()
             
         self.M = np.sqrt(M_norm)
-
-        self.M_t = qtn.Tensor(self.M, inds=[f"k{i}" for i in range(self.M.ndim)], tags = "M")
+        # self.M = self.M - self.M.mean()
+        # self.M = M_norm
 
         self.built = True
 
-    def partial_trace(self, index):
+    def partial_trace(self, terms):
+
+        if isinstance(terms, list):
+            self.terms = terms
+        else:
+            self.terms = [terms]
 
         if not self.built:
             return "SLG [E]: Tensor not built. Run the `build()` method on the object"
-        if index >= len(self.M_t.inds):
-            return "SLG [E]: The index is greater than the tensor rank"
-        
-        self.M_tH = self.M_t.H
-        self.M_tH.drop_tags("M")
-        self.M_tH.add_tag("Mh")
 
-        self.M_tH.reindex({ind:(ind if i!= index else f"cut_{index}") for i,ind in enumerate(self.M_tH.inds)}, inplace=True)
-        self.M_tn = self.M_t & self.M_tH
-        self.M_pt = self.M_tn^...
-        self.M_pt = self.M_pt.data
+        if max(self.terms) >= self.M.ndim:
+            return "SLG [E]: At least one term index is greater than the tensor rank"
+        
+        self.contexts = [i for i in range(self.M.ndim) if i not in set(self.terms)]
+
+        self.M_pt = np.tensordot(self.M, self.M.T, axes=(self.contexts,self.contexts[::-1]))
 
         self.plot_pt = plot_hm(
             z = self.M_pt,
             x = self.elements,
             y = self.elements)
+
+    def pt_svd(self, truncate = None):
+        # Taken from Zichen Wang (https://towardsdatascience.com/turbocharging-svd-with-jax-749ae12f93af)
+
+        # Compute full SVD
+        if self.M_pt.ndim > 2:
+            "SLG [E]: The partial trace is a tensor of rank > 2. SVD is not implemented for these cases"
+        self.pt_U, self.pt_s, self.pt_Vh = jnp.linalg.svd(self.M_pt, 
+                                    full_matrices=False, # It's not necessary to compute the full matrix of U or V
+                                    compute_uv=True,
+                                    )
+        self.plot_pt_svd = plot_hm(
+            z = jnp.diag(self.pt_s) @ self.pt_Vh,
+            x = self.elements,
+            y = [f"t_{i}" for i in range(self.dims)])
+
